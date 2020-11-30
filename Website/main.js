a612a0986ac8a47ed2c9aced88d7535ea980430d
Jayvee <imjyvee@gmail.com>
2020-11-30 21:10:54 +0800

    add

--- Website/main.js
+++ Website/main.js
@@ -2,10 +2,16 @@ function validate() {
     var name = document.getElementById('name').value;
     var password = document.getElementById('password').value;
 
-    if(name=='samantha' || password=='mahalko') {
-        return true;
-    }
-    else {
+    if(name=='' || password=='') {
+        document.getElementById("form__message--error").innerText = "All fields are required";
         return false;
+    }
+    else if (name!='samantha' || password!= 'mahalko') {
+        document.getElementById("form__message--error").innerText = " Incorrect Email or Password";
+        return false;
+    }
+
+    else {
+        return true;
     }
 }
